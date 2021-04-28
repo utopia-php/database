78ed2c00686fef5ff38f31671820da3124c84d75
kodumbeats <brandon.leckemby@gmail.com>
2021-04-28 17:29:12 -0400

    Test query validator

---
+++ tests/Database/Validator/QueryValidatorTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Utopia\Tests\Validator;
+
+use Utopia\Database\Validator\QueryValidator;
+use PHPUnit\Framework\TestCase;
+use Utopia\Database\Database;
+use Utopia\Database\Query;
+
+class QueryValidatorTest extends TestCase
+{
+    /**
+     * @var array 
+     */
+    protected $schema = [
+        [
+            '$id' => 'title',
+            'type' => Database::VAR_STRING,
+            'size' => 256,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'description',
+            'type' => Database::VAR_STRING,
+            'size' => 1000000,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'rating',
+            'type' => Database::VAR_INTEGER,
+            'size' => 5,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'price',
+            'type' => Database::VAR_FLOAT,
+            'size' => 5,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'published',
+            'type' => Database::VAR_BOOLEAN,
+            'size' => 5,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'tags',
+            'type' => Database::VAR_STRING,
+            'size' => 55,
+            'required' => true,
+            'signed' => true,
+            'array' => true,
+            'filters' => [],
+        ],
+    ];
+
+    public function setUp(): void
+    {
+    }
+
+    public function tearDown(): void
+    {
+    }
+
+    public function testQuery()
+    {
+        $validator = new QueryValidator($this->schema);
+
+        $query = Query::parse('title.equal("Iron Man")');
+        var_dump($query);
+
+        $this->assertEquals(true, $validator->isValid($query));
+    }
+
+}

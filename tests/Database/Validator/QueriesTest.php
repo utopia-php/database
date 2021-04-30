5869e492342f523e36e21e29f9e2d65062675d5b
kodumbeats <brandon.leckemby@gmail.com>
2021-04-30 17:24:45 -0400

    Test Queries validator

--- tests/Database/Validator/QueriesTest.php
+++ tests/Database/Validator/QueriesTest.php
@@ -10,6 +10,66 @@ use Utopia\Database\Query;
 class QueriesTest extends TestCase
 {
     /**
+     * @var array 
+     */
+    public $schema = [
+        [
+            '$id' => 'title',
+            'type' => Database::VAR_STRING,
+            'size' => 256,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'description',
+            'type' => Database::VAR_STRING,
+            'size' => 1000000,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'rating',
+            'type' => Database::VAR_INTEGER,
+            'size' => 5,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'price',
+            'type' => Database::VAR_FLOAT,
+            'size' => 5,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'published',
+            'type' => Database::VAR_BOOLEAN,
+            'size' => 5,
+            'required' => true,
+            'signed' => true,
+            'array' => false,
+            'filters' => [],
+        ],
+        [
+            '$id' => 'tags',
+            'type' => Database::VAR_STRING,
+            'size' => 55,
+            'required' => true,
+            'signed' => true,
+            'array' => true,
+            'filters' => [],
+        ],
+    ];
+
+    /**
      * @var array
      */
     protected $indexes = [
@@ -39,8 +99,26 @@ class QueriesTest extends TestCase
         ],
     ];
 
+    /**
+     * @var Query[] $queries
+     */
+    protected $queries = [];
+
+    /**
+     * @var QueryValidator
+     */
+    protected $queryValidator;
+
     public function setUp(): void
     {
+        $this->queryValidator= new QueryValidator($this->schema);
+
+        $query1 = Query::parse('title.notEqual("Iron Man", "Ant Man")');
+        $query2 = Query::parse('description.equal("Best movie ever")');
+        $query3 = Query::parse('rating.greater(4)');
+        $query4 = Query::parse('price.lesserEqual(6.50)');
+
+        array_push($queries, $query1, $query2, $query3, $query4);
     }
 
     public function tearDown(): void
@@ -49,6 +127,8 @@ class QueriesTest extends TestCase
 
     public function testQueries()
     {
-    }
+        $validator = new Queries($this->validator, $this->indexes);
 
+        $this->assertEquals(1, 1);
+    }
 }

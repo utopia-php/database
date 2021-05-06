a4e4c61cf600e613a477033343c5e514f18fa49b
kodumbeats <brandon.leckemby@gmail.com>
2021-05-06 11:23:19 -0400

    Outline mongodb capabilities

--- src/Database/Adapter/MongoDB.php
+++ src/Database/Adapter/MongoDB.php
@@ -1,134 +1,185 @@
 <?php
 
-// namespace Utopia\Database\Adapter;
+namespace Utopia\Database\Adapter;
 
-// use Exception;
-// use Utopia\Database\Adapter;
-// use MongoDB\Client;
-// use MongoDB\Database;
+use Exception;
+use Utopia\Database\Adapter;
+use MongoDB\Client;
+use MongoDB\Database;
 
-// class MongoDB extends Adapter
-// {
-//     /**
-//      * @var Client
-//      */
-//     protected $client;
+class MongoDB extends Adapter
+{
+    /**
+     * @var Client
+     */
+    protected $client;
 
-//     /**
-//      * @var Database|null
-//      */
-//     protected $database;
+    /**
+     * @var Database|null
+     */
+    protected $database;
 
-//     /**
-//      * Constructor.
-//      *
-//      * Set connection and settings
-//      *
-//      * @param Client $client
-//      */
-//     public function __construct(Client $client)
-//     {
-//         $this->client = $client;
-//     }
+    /**
+     * Constructor.
+     *
+     * Set connection and settings
+     *
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
 
-//     /**
-//      * Create Database
-//      * 
-//      * @return bool
-//      */
-//     public function create(): bool
-//     {
-//         $namespace = $this->getNamespace();
-//         return (!!$this->client->$namespace);
-//     }
+    /**
+     * Create Database
+     * 
+     * @return bool
+     */
+    public function create(): bool
+    {
+        $namespace = $this->getNamespace();
+        return (!!$this->client->$namespace);
+    }
 
-//     /**
-//      * List Databases
-//      * 
-//      * @return array
-//      */
-//     public function list(): array
-//     {
-//         $list = [];
+    /**
+     * List Databases
+     * 
+     * @return array
+     */
+    public function list(): array
+    {
+        $list = [];
 
-//         foreach ($this->client->listDatabaseNames() as $key => $value) {
-//             $list[] = $value;
-//         }
+        foreach ($this->client->listDatabaseNames() as $key => $value) {
+            $list[] = $value;
+        }
         
-//         return $list;
-//     }
+        return $list;
+    }
 
-//     /**
-//      * Delete Database
-//      * 
-//      * @return bool
-//      */
-//     public function delete(): bool
-//     {
-//         return (!!$this->getDatabase()->dropCollection($this->getNamespace()));
-//     }
+    /**
+     * Delete Database
+     * 
+     * @return bool
+     */
+    public function delete(): bool
+    {
+        return (!!$this->getDatabase()->drop($this->getNamespace()));
+    }
 
-//     /**
-//      * Create Collection
-//      * 
-//      * @param string $id
-//      * @return bool
-//      */
-//     public function createCollection(string $id): bool
-//     {
-//         return (!!$this->getDatabase()->createCollection($id));
-//     }
+    /**
+     * Create Collection
+     * 
+     * @param string $id
+     * @return bool
+     */
+    public function createCollection(string $id): bool
+    {
+        return (!!$this->getDatabase()->createCollection($id));
+    }
 
-//     /**
-//      * List Collections
-//      * 
-//      * @return array
-//      */
-//     public function listCollections(): array
-//     {
-//         $list = [];
+    /**
+     * List Collections
+     * 
+     * @return array
+     */
+    public function listCollections(): array
+    {
+        $list = [];
 
-//         foreach ($this->getDatabase()->listCollectionNames() as $key => $value) {
-//             $list[] = $value;
-//         }
+        foreach ($this->getDatabase()->listCollectionNames() as $key => $value) {
+            $list[] = $value;
+        }
         
-//         return $list;
-//     }
+        return $list;
+    }
 
-//     /**
-//      * Delete Collection
-//      * 
-//      * @param string $id
-//      * @return bool
-//      */
-//     public function deleteCollection(string $id): bool
-//     {
-//         return (!!$this->getDatabase()->dropCollection($id));
-//     }
+    /**
+     * Delete Collection
+     * 
+     * @param string $id
+     * @return bool
+     */
+    public function deleteCollection(string $id): bool
+    {
+        return (!!$this->getDatabase()->dropCollection($id));
+    }
 
-//     /**
-//      * @return Database
-//      *
-//      * @throws Exception
-//      */
-//     protected function getDatabase()
-//     {
-//         if($this->database) {
-//             return $this->database;
-//         }
+    /**
+     * @return Database
+     *
+     * @throws Exception
+     */
+    protected function getDatabase()
+    {
+        if($this->database) {
+            return $this->database;
+        }
 
-//         $namespace = $this->getNamespace();
+        $namespace = $this->getNamespace();
         
-//         return $this->client->$namespace;
-//     }
+        return $this->client->$namespace;
+    }
 
-//     /**
-//      * @return Client
-//      *
-//      * @throws Exception
-//      */
-//     protected function getClient()
-//     {
-//         return $this->client;
-//     }
-// }
+    /**
+     * @return Client
+     *
+     * @throws Exception
+     */
+    protected function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Get max STRING limit
+     * 
+     * @return int
+     */
+    public function getStringLimit(): int
+    {
+        return 2147483647;
+    }
+
+    /**
+     * Get max INT limit
+     * 
+     * @return int
+     */
+    public function getIntLimit(): int
+    {
+        // Mongo does not handle integers directly, so using MariaDB limit for now
+        return 4294967295; 
+    }
+
+    /**
+     * Is index supported?
+     * 
+     * @return bool
+     */
+    public function getSupportForIndex(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Is unique index supported?
+     * 
+     * @return bool
+     */
+    public function getSupportForUniqueIndex(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Is fulltext index supported?
+     * 
+     * @return bool
+     */
+    public function getSupportForFulltextIndex(): bool
+    {
+        return true;
+    }
+}

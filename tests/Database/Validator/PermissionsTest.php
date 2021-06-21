de91ce2049cc5d6b9c3c29c6b2fed731aca7745f
kodumbeats <brandon.leckemby@gmail.com>
2021-06-21 17:29:33 -0400

    Test for invalid types

--- tests/Database/Validator/PermissionsTest.php
+++ tests/Database/Validator/PermissionsTest.php
@@ -44,4 +44,26 @@ class PermissionsTest extends TestCase
         $this->assertEquals($object->isValid('sting'), false);
         
     }
+
+    public function testInvalidPermissions()
+    {
+        $object = new Permissions();
+
+        // Must be array of strings
+        $this->assertEquals($object->isValid('role:all'), false);
+        $this->assertEquals($object->getDescription(), 'Permissions roles must be an array of strings.');
+        $this->assertEquals($object->isValid(false), false);
+        $this->assertEquals($object->getDescription(), 'Permissions roles must be an array of strings.');
+        $this->assertEquals($object->isValid(1.5), false);
+        $this->assertEquals($object->getDescription(), 'Permissions roles must be an array of strings.');
+
+        // Permissions role must be of type string
+        $this->assertEquals($object->isValid([0]), false);
+        $this->assertEquals($object->getDescription(), 'Permissions role must be of type string.');
+        $this->assertEquals($object->isValid([false]), false);
+        $this->assertEquals($object->getDescription(), 'Permissions role must be of type string.');
+        $this->assertEquals($object->isValid([['a']]), false);
+        $this->assertEquals($object->getDescription(), 'Permissions role must be of type string.');
+
+    }
 }

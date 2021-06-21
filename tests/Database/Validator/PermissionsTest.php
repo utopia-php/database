b9bec38f05f2d605dfed4eb964ce8024faf4528c
kodumbeats <brandon.leckemby@gmail.com>
2021-06-21 17:41:26 -0400

    Test for wildcard char, colons, and types of roles

--- tests/Database/Validator/PermissionsTest.php
+++ tests/Database/Validator/PermissionsTest.php
@@ -58,12 +58,33 @@ class PermissionsTest extends TestCase
         $this->assertEquals($object->getDescription(), 'Permissions roles must be an array of strings.');
 
         // Permissions role must be of type string
-        $this->assertEquals($object->isValid([0]), false);
+        $this->assertEquals($object->isValid([0, 1.5]), false);
         $this->assertEquals($object->getDescription(), 'Permissions role must be of type string.');
-        $this->assertEquals($object->isValid([false]), false);
+        $this->assertEquals($object->isValid([false, []]), false);
         $this->assertEquals($object->getDescription(), 'Permissions role must be of type string.');
         $this->assertEquals($object->isValid([['a']]), false);
         $this->assertEquals($object->getDescription(), 'Permissions role must be of type string.');
 
+        // Wildcard character deprecated
+        $this->assertEquals($object->isValid(['*']), false);
+        $this->assertEquals($object->getDescription(), 'Wildcard permission "*" deprecated. Use "role:all" instead.');
+
+        // Only contains a single ':'
+        $this->assertEquals($object->isValid(['user1234']), false);
+        $this->assertEquals($object->getDescription(), 'Permission role must contain one and only one \':\' character: user1234');
+        $this->assertEquals($object->isValid(['user::1234']), false);
+        $this->assertEquals($object->getDescription(), 'Permission role must contain one and only one \':\' character: user::1234');
+        $this->assertEquals($object->isValid(['user:123:4']), false);
+        $this->assertEquals($object->getDescription(), 'Permission role must contain one and only one \':\' character: user:123:4');
+
+        // Permission role must begin with one of: member, role, team, user
+        $this->assertEquals($object->isValid(['memmber:1234']), false);
+        $this->assertEquals($object->getDescription(), 'Permission role must begin with one of: member, role, team, user');
+        $this->assertEquals($object->isValid(['rol:1234']), false);
+        $this->assertEquals($object->getDescription(), 'Permission role must begin with one of: member, role, team, user');
+        $this->assertEquals($object->isValid(['tteam:1234']), false);
+        $this->assertEquals($object->getDescription(), 'Permission role must begin with one of: member, role, team, user');
+        $this->assertEquals($object->isValid(['userr:1234']), false);
+        $this->assertEquals($object->getDescription(), 'Permission role must begin with one of: member, role, team, user');
     }
 }

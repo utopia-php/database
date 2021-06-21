e9d8d22be8795732ce2a36bfd0f1e42b8a1bed52
kodumbeats <brandon.leckemby@gmail.com>
2021-06-21 17:28:11 -0400

    Check for wildcard, colon, and given permission are valid

--- src/Database/Validator/Permissions.php
+++ src/Database/Validator/Permissions.php
@@ -12,6 +12,16 @@ class Permissions extends Validator
     protected $message = 'Permissions Error';
 
     /**
+     * @var string[]
+     */
+    protected $permissions = [
+        'member',
+        'role',
+        'team',
+        'user',
+    ];
+
+    /**
      * Get Description.
      *
      * Returns validator description
@@ -35,13 +45,34 @@ class Permissions extends Validator
     public function isValid($roles)
     {
         if(!is_array($roles)) {
-            $this->message = 'Permissions roles must be an array of strings';
+            $this->message = 'Permissions roles must be an array of strings.';
             return false;
         }
 
         foreach ($roles as $role) {
             if (!\is_string($role)) {
                 $this->message = 'Permissions role must be of type string.';
+
+                return false;
+            }
+
+            if ($role === '*') {
+                $this->message = 'Wildcard permission "*" deprecated. Use "role:all" instead.';
+
+                return false;
+            }
+
+            $pos = \strpos($role, ':');
+            // Should only contain a single ":" char
+            if ($pos === false || $pos !== \strrpos($role, ':')) {
+                $this->message = 'Permission role must contain one and only one \':\' character: ' . $role;
+
+                return false;
+            }
+
+            // Substring before ":" must be a known permission
+            if (!\in_array(\substr($role, 0, \strpos($role, ":")), $this->permissions)) {
+                $this->message = 'Permission role must begin with one of: ' . \implode(", ", $this->permissions);
 
                 return false;
             }

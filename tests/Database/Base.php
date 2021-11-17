ff6c5965a7a3dc524baf0624d5d1e264795a3cf7
kodumbeats <brandon.leckemby@gmail.com>
2021-11-17 14:56:50 -0500

    Test that OR queries use the correct parentheses

--- tests/Database/Base.php
+++ tests/Database/Base.php
@@ -916,17 +916,6 @@ abstract class Base extends TestCase
         $this->assertEmpty(count($documents));
 
         /**
-         * ORDER BY - After Exception
-         */
-
-        $document = new Document([
-            '$collection' => 'other collection'
-        ]);
-
-        $this->expectException(Exception::class);
-        static::getDatabase()->find('movies', [], 2, 0, [], [], $document);
-
-        /**
          * Limit
          */
         $documents = static::getDatabase()->find('movies', [], 4, 0, ['name']);
@@ -947,6 +936,26 @@ abstract class Base extends TestCase
         $this->assertEquals('Frozen II', $documents[1]['name']);
         $this->assertEquals('Work in Progress', $documents[2]['name']);
         $this->assertEquals('Work in Progress 2', $documents[3]['name']);
+
+        /**
+         * Test that OR queries are handled correctly
+         */
+        $documents = static::getDatabase()->find('movies', [
+            new Query('director', Query::TYPE_EQUAL, ['TBD', 'Joe Johnston']),
+            new Query('year', Query::TYPE_EQUAL, [2025]),
+        ]);
+        $this->assertEquals(1, count($documents));
+
+        /**
+         * ORDER BY - After Exception
+         * Must be last assertion in test
+         */
+        $document = new Document([
+            '$collection' => 'other collection'
+        ]);
+
+        $this->expectException(Exception::class);
+        static::getDatabase()->find('movies', [], 2, 0, [], [], $document);
     }
 
     /**
@@ -984,6 +993,17 @@ abstract class Base extends TestCase
         Authorization::disable();
         $count = static::getDatabase()->count('movies', [], 3);
         $this->assertEquals(3, $count);
+        Authorization::reset();
+
+        /**
+         * Test that OR queries are handled correctly
+         */
+        Authorization::disable();
+        $count = static::getDatabase()->count('movies', [
+            new Query('director', Query::TYPE_EQUAL, ['TBD', 'Joe Johnston']),
+            new Query('year', Query::TYPE_EQUAL, [2025]),
+        ]);
+        $this->assertEquals(1, $count);
         Authorization::reset();
     }
 

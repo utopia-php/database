75af7756bd3924762a6717bcc16e2506a2ccd806
Kevin Joshi <kevinjoshi46b@gmail.com>
2022-03-21 12:54:31 +0530

    feat: Enabled Tests for cockroach adapter
    - Uncommented and modified the CockroachTest.php file to run tests for cockroach adapter

--- tests/Database/Adapter/CockroachTest.php
+++ tests/Database/Adapter/CockroachTest.php
@@ -1,46 +1,74 @@
 <?php
 
-// namespace Utopia\Tests\Adapter;
+namespace Utopia\Tests\Adapter;
 
-// use PDO;
-// use Utopia\Database\Adapter\Cockroach;
-// use Utopia\Database\Database;
-// use Utopia\Tests\Base;
+use PDO;
+use Redis;
+use Utopia\Database\Adapter\Cockroach;
+use Utopia\Database\Database;
+use Utopia\Tests\Base;
+use Utopia\Cache\Cache;
+use Utopia\Cache\Adapter\Redis as RedisAdapter;
 
-// class CockroachTest extends Base
-// {
-//     /**
-//      * @var Database
-//      */
-//     static $database = null;
+class CockroachTest extends Base
+{
+    /**
+     * @var Database
+     */
+    static $database = null;
 
-//     /**
-//      * @reture Adapter
-//      */
-//     static function getDatabase(): Database
-//     {
-//         if(!is_null(self::$database)) {
-//             return self::$database;
-//         }
+    /**
+     * Return name of adapter
+     *
+     * @return string
+     */
+    static function getAdapterName(): string
+    {
+        return "cockroach";
+    }
 
-//         $dbHost = 'cockroach';
-//         $dbPort = '26257';
-//         $dbUser = null;
-//         $dbPass = null;
+    /**
+     * Return row limit of adapter
+     *
+     * @return int
+     */
+    static function getAdapterRowLimit(): int
+    {
+        return Cockroach::getRowLimit();
+    }
 
-//         $pdo = new PDO("pgsql:host={$dbHost};port={$dbPort};", $dbUser, $dbPass, array(
-//             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
-//             PDO::ATTR_TIMEOUT => 3, // Seconds
-//             PDO::ATTR_PERSISTENT => true
-//         ));
+    /**
+     * @return Adapter
+     */
+    static function getDatabase(): Database
+    {
+        if (!is_null(self::$database)) {
+            return self::$database;
+        }
 
-//         // Connection settings
-//         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);   // Return arrays
-//         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        // Handle all errors with exceptions
+        $dbHost = 'cockroach';
+        $dbPort = '26257';
+        $dbUser = null;
+        $dbPass = null;
 
-//         $database = new Database(new Cockroach($pdo));
-//         $database->setNamespace('myapp_'.uniqid());
+        $pdo = new PDO("pgsql:host={$dbHost};port={$dbPort};", $dbUser, $dbPass, array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
+            PDO::ATTR_TIMEOUT => 3, // Seconds
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => true
+        ));
 
-//         return self::$database = $database;
-//     }
-// }
+        $redis = new Redis();
+        $redis->connect('redis', 6379);
+        $redis->flushAll();
+        $cache = new Cache(new RedisAdapter($redis));
+
+        $database = new Database(new Cockroach($pdo), $cache);
+        $database->setDefaultDatabase('utopiaTests');
+        $database->setNamespace('myapp_' . uniqid());
+
+        return self::$database = $database;
+    }
+}

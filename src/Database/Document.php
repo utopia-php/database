69c8d40086d278e327240e1b74e85c756a47a1ff
Matej Baco <matejbacocom@gmail.com>
2022-04-19 11:02:13 +0200

    Fix array_column

--- src/Database/Document.php
+++ src/Database/Document.php
@@ -50,6 +50,20 @@ class Document extends ArrayObject
     }
 
     /**
+     * Methods __get and __isset allows usage of array_column() above Document class.
+     * Refference:  https://www.php.net/manual/en/function.array-column.php (Example #4 )
+     */
+    public function __get($prop)
+    {
+        return $this->getAttribute($prop, null);
+    }
+
+    public function __isset($prop) : bool
+    {
+        return $this->getAttribute($prop, null) !== null;
+    }
+
+    /**
      * @return string
      */
     public function getId(): string

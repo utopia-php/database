9d7afdf68eac546df7584176a14cea12828ce73e
wess <wcope@me.com>
2022-05-03 15:33:43 -0400

    Removes debugging stuff from Database.php

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -1070,7 +1070,6 @@ class Database
 
         $this->cache->save('cache-' . $this->getNamespace() . ':' . $collection->getId() . ':' . $id, $document->getArrayCopy()); // save to cache after fetching from db
 
-        var_dump($document);
         return $document;
     }
 
@@ -1140,9 +1139,6 @@ class Database
         $validator = new Authorization('write');
 
         if (!$validator->isValid($old->getWrite())) { // Check if user has write access to this document
-          var_dump($old);
-          die;
-
             throw new AuthorizationException($validator->getDescription());
         }
 

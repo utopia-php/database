7eb0ac7b51bc0a4f28579b8a693384baeae24ebe
wess <wcope@me.com>
2022-05-10 11:03:22 -0400

    Working on case issue where authorization fails

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -857,6 +857,8 @@ class Database
 
         $validator = new Authorization('write');
 
+        
+
         if (!$validator->isValid($old->getWrite())) { // Check if user has write access to this document
             throw new AuthorizationException($validator->getDescription());
         }

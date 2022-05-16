381b4628b5d82a898d06275ad08bc4c21619bb84
wess <wcope@me.com>
2022-05-16 10:27:00 -0400

    Updates composer

--- tests/Database/Adapter/MongoDBTest.php
+++ tests/Database/Adapter/MongoDBTest.php
@@ -15,10 +15,82 @@ use Utopia\Tests\Base;
 /*
 class MongoDBTest extends Base
 {
+    static $pool = null;
+
     /**
      * @var Database
      * /
     static $database = null;
+
+    public function setUp(): void {
+      // super::setUp(); 
+
+      self::getPool();
+      self::setupDB();
+    }
+
+    static function getPool() {
+      if(self::$pool) {
+        return self::$pool;
+      }
+
+      MongoDBTest::$pool = new \Swoole\ConnectionPool(function() {
+        $options = new MongoClientOptions(
+          'utopia_testing',
+          'mongo',
+          27017,
+          'root',
+          'example'
+      );
+
+        $client = new MongoClient($options);
+
+        $redis = new Redis();
+        $redis->connect('redis', 6379);
+        $redis->flushAll();
+        $cache = new Cache(new RedisAdapter($redis));
+
+  
+        return $client;
+      });
+
+      
+      return self::$pool;
+    }
+
+    static function setupDB() {
+      $pool = self::getPool();
+      
+      $db = null;
+
+      $res = \Swoole\Coroutine\run(function() use ($pool) {
+        
+        $client = $pool->get();
+
+        $redis = new Redis();
+        $redis->connect('redis', 6379);
+        $redis->flushAll();
+        $cache = new Cache(new RedisAdapter($redis));
+
+        $client = self::getPool()->get();
+        $database = new Database(new MongoDBAdapter($client), $cache);
+        $database->setDefaultDatabase('utopiaTests');
+        $database->setNamespace('myapp_'.uniqid());
+
+        
+        $pool->put($client);
+
+        $db = $database;
+
+        \Swoole\Coroutine\Client\close();
+      });
+
+      var_dump($db);
+
+      while($db == null){}
+
+      //self::$database = $channel->pop();
+    }
 
     // TODO@kodumbeats hacky way to identify adapters for tests
     // Remove once all methods are implemented
@@ -47,30 +119,25 @@ class MongoDBTest extends Base
      * /
     static function getDatabase(): Database
     {
-        if(!is_null(self::$database)) {
-            return self::$database;
-        }
-
-        $options = new MongoClientOptions(
-            'utopia_testing',
-            'mongo',
-            27017,
-            'root',
-            'example'
-        );
-
-        $client = new MongoClient($options);
+        // if(!is_null(self::$database)) {
+        //     return self::$database;
+        // }
         
-        $redis = new Redis();
-        $redis->connect('redis', 6379);
-        $redis->flushAll();
-        $cache = new Cache(new RedisAdapter($redis));
+        // $redis = new Redis();
+        // $redis->connect('redis', 6379);
+        // $redis->flushAll();
+        // $cache = new Cache(new RedisAdapter($redis));
 
-        $database = new Database(new MongoDBAdapter($client), $cache);
-        $database->setDefaultDatabase('utopiaTests');
-        $database->setNamespace('myapp_'.uniqid());
+        // $client = self::getPool()->get();
+        // $database = new Database(new MongoDBAdapter($client), $cache);
+        // $database->setDefaultDatabase('utopiaTests');
+        // $database->setNamespace('myapp_'.uniqid());
 
-        return self::$database = $database;
+
+        // self::getPool()->put($client);
+
+        // return self::$database = $database;
+        return self::$database;
     }
 }
 */

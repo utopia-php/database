091302be105758c866d045545dabdb767c58914d
Matej Bačo <matejbaco2000@gmail.com>
2022-06-20 08:35:15 +0000

    Add new test case

--- tests/Database/Base.php
+++ tests/Database/Base.php
@@ -1229,6 +1229,19 @@ abstract class Base extends TestCase
         $this->assertEmpty(count($documents));
 
         /**
+         * ORDER BY + CURSOR
+         */
+        $documentsTest = static::getDatabase()->find('movies', [], 2, 0, ['$id'], [Database::ORDER_DESC]);
+        $documents = static::getDatabase()->find('movies', [], 1, 0, ['$id'], [Database::ORDER_DESC], $documentsTest[0], Database::CURSOR_AFTER);
+
+        $this->assertEquals($documentsTest[1]['$id'], $documents[0]['$id']);
+        $this->assertEquals($documentsTest[1]['name'], $documents[0]['name']);
+        $this->assertEquals($documentsTest[1]['price'], $documents[0]['price']);
+        $this->assertEquals($documentsTest[1]['year'], $documents[0]['year']);
+
+        die();
+
+        /**
          * Limit
          */
         $documents = static::getDatabase()->find('movies', [], 4, 0, ['name']);

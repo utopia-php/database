56618ddc79a4fce4389b27a9730da81035d2ffa6
wess <wcope@me.com>
2022-07-27 16:05:58 -0400

    Mistyped var

--- src/Database/Adapter/Mongo/MongoClient.php
+++ src/Database/Adapter/Mongo/MongoClient.php
@@ -569,7 +569,7 @@ class MongoClient
         $obj = new \stdClass();
 
         foreach ($dict as $k => $v) {
-            $obj->{$key} = \is_array($v) ? $this->toObject($v) : $v;
+            $obj->{$k} = \is_array($v) ? $this->toObject($v) : $v;
         }
 
         return $obj;

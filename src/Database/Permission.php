91a4e8cf353b49034e18a13627cf2c6613f9905a
Jake Barnby <jakeb994@gmail.com>
2022-08-12 23:41:43 +1200

    Add permissions helper classes

---
+++ src/Database/Permission.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Utopia\Database;
+
+class Permission
+{
+    private Role $role;
+
+    public function __construct(
+        private string $permission,
+        string $role,
+        string $identifier = '',
+        string $dimension = '',
+    )
+    {
+        $this->role = new Role($role, $identifier, $dimension);
+    }
+
+    public static function fromString(string $permission): Permission
+    {
+        $parts = \explode('(', $permission);
+        $permission = $parts[0];
+        $fullRole = \str_replace(')', '', $parts[1]);
+        $parts = \explode(':', $fullRole);
+
+        if (\count($parts) === 1) {
+            return new Permission($permission, $fullRole);
+        }
+        $role = $parts[0];
+        $fullIdentifier = $parts[1];
+        $parts = \explode('/', $fullIdentifier);
+
+        if (\count($parts) === 1) {
+            return new Permission($permission, $role, $fullIdentifier);
+        }
+        $identifier = $parts[0];
+        $dimension = $parts[1];
+
+        return new Permission(
+            $permission,
+            $role,
+            $identifier,
+            $dimension,
+        );
+    }
+
+    /**
+     * @param Role $role
+     * @return Permission
+     */
+    public static function read(Role $role): Permission
+    {
+        return new Permission('read', $role->getRole(), $role->getIdentifier(), $role->getDimension());
+    }
+
+    /**
+     * @param Role $role
+     * @return Permission
+     */
+    public static function create(Role $role): Permission
+    {
+        return new Permission('create', $role->getRole(), $role->getIdentifier(), $role->getDimension());
+    }
+
+    /**
+     * @param Role $role
+     * @return Permission
+     */
+    public static function update(Role $role): Permission
+    {
+        return new Permission('update', $role->getRole(), $role->getIdentifier(), $role->getDimension());
+    }
+
+    /**
+     * @param Role $role
+     * @return Permission
+     */
+    public static function delete(Role $role): Permission
+    {
+        return new Permission('delete', $role->getRole(), $role->getIdentifier(), $role->getDimension());
+    }
+
+    /**
+     * @return string
+     */
+    public function toString(): string
+    {
+        return $this->permission . '(' . $this->role->toString() . ')';
+    }
+
+    /**
+     * @return string
+     */
+    public function getPermission(): string
+    {
+        return $this->permission;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role->getRole();
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return $this->role->getIdentifier();
+    }
+
+    /**
+     * @return string
+     */
+    public function getDimension(): string
+    {
+        return $this->role->getDimension();
+    }
+}
+
---
+++ src/Database/Role.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Utopia\Database;
+
+class Role
+{
+    public function __construct(
+        private string $role,
+        private string $identifier = '',
+        private string $dimension = '',
+    )
+    {
+    }
+
+    public static function fromString(string $role): Role
+    {
+        $parts = \explode(':', $role);
+
+        if (\count($parts) === 1) {
+            return new Role($role);
+        }
+        $role = $parts[0];
+        $fullIdentifier = $parts[1];
+        $parts = \explode('/', $fullIdentifier);
+
+        if (\count($parts) === 1) {
+            return new Role($role, $fullIdentifier);
+        }
+        $identifier = $parts[0];
+        $dimension = $parts[1];
+
+        return new Role(
+            $role,
+            $identifier,
+            $dimension,
+        );
+    }
+
+    public static function user(string $identifier): Role
+    {
+        return new Role('user', $identifier);
+    }
+
+    public static function users(): Role
+    {
+        return new Role('users');
+    }
+
+    public static function team(string $identifier, string $dimension = ''): Role
+    {
+        return new Role('team', $identifier, $dimension);
+    }
+
+    public static function any(): Role
+    {
+        return new Role('any');
+    }
+
+    public static function guests(): Role
+    {
+        return new Role('guests');
+    }
+
+    public function toString(): string
+    {
+        $str = $this->role;
+        if ($this->identifier) {
+            $str .= ':' . $this->identifier;
+        }
+        if ($this->dimension) {
+            $str .= '/' . $this->dimension;
+        }
+        return $str;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDimension(): string
+    {
+        return $this->dimension;
+    }
+}

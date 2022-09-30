642ce0a61dd9cc3a3ab46641444a3f99c25b6c5d
Eldad Fux <eldad.fux@gmail.com>
2022-09-30 14:43:11 +0300

    Added PDO config

--- tests/Database/Adapter/PostgresTest.php
+++ tests/Database/Adapter/PostgresTest.php
@@ -51,14 +51,7 @@ class PostgresTest extends Base
         $dbUser = 'root';
         $dbPass = 'password';
 
-        $pdo = new PDO("pgsql:host={$dbHost};port={$dbPort};", $dbUser, $dbPass, [
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
-            PDO::ATTR_TIMEOUT => 3, // Seconds
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_EMULATE_PREPARES => true
-        ]);
+        $pdo = new PDO("pgsql:host={$dbHost};port={$dbPort};", $dbUser, $dbPass, Postgres::getPDOAttributes());
         $redis = new Redis();
         $redis->connect('redis', 6379);
         $redis->flushAll();

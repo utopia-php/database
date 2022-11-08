051078fa6ec1bdbc3d35c170096656180976cb04
fogelito <fogelshmuel@gmail.com>
2022-11-08 15:56:09 +0200

    change function to public

--- src/Database/Adapter.php
+++ src/Database/Adapter.php
@@ -504,5 +504,5 @@ abstract class Adapter
      * @return bool
      * @throws Exception
      */
-    abstract function increaseDocumentAttribute(string $collection, string $id, string $attribute, int|float $value, int|float|null $min = null, int|float|null $max = null):bool;
+    abstract public function increaseDocumentAttribute(string $collection, string $id, string $attribute, int|float $value, int|float|null $min = null, int|float|null $max = null):bool;
 }
--- src/Database/Adapter/Mongo/MongoDBAdapter.php
+++ src/Database/Adapter/Mongo/MongoDBAdapter.php
@@ -1105,8 +1105,7 @@ class MongoDBAdapter extends Adapter
      * @return bool
      * @throws Exception
      */
-    function increaseDocumentAttribute(string $collection, string $id, string $attribute, int|float $value, int|float|null $min = null, int|float|null $max = null):bool {
-
+    public function increaseDocumentAttribute(string $collection, string $id, string $attribute, int|float $value, int|float|null $min = null, int|float|null $max = null):bool {
         $attribute = $this->filter($attribute);
         $where = ['_uid' => $id];
 

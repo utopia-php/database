6034b66d67ebb7730d6dbadbf71ce17917d06406
fogelito <fogelshmuel@gmail.com>
2023-02-12 12:08:23 +0200

    run tests

--- tests/Database/Adapter/PostgresTest.php
+++ tests/Database/Adapter/PostgresTest.php
@@ -1,56 +1,56 @@
 <?php
-//
-//namespace Utopia\Tests\Adapter;
-//
-//use PDO;
-//use Redis;
-//use Utopia\Database\Database;
-//use Utopia\Database\Adapter\Postgres;
-//use Utopia\Cache\Cache;
-//use Utopia\Cache\Adapter\Redis as RedisAdapter;
-//use Utopia\Tests\Base;
-//
-//class PostgresTest extends Base
-//{
-//    /**
-//     * @var Database
-//     */
-//    static $database = null;
-//
-//    /**
-//     * Return name of adapter
-//     *
-//     * @return string
-//     */
-//    static function getAdapterName(): string
-//    {
-//        return "postgres";
-//    }
-//
-//    /**
-//     * @reture Adapter
-//     */
-//    static function getDatabase(): Database
-//    {
-//        if(!is_null(self::$database)) {
-//            return self::$database;
-//        }
-//
-//        $dbHost = 'postgres';
-//        $dbPort = '5432';
-//        $dbUser = 'root';
-//        $dbPass = 'password';
-//
-//        $pdo = new PDO("pgsql:host={$dbHost};port={$dbPort};", $dbUser, $dbPass, Postgres::getPDOAttributes());
-//        $redis = new Redis();
-//        $redis->connect('redis', 6379);
-//        $redis->flushAll();
-//        $cache = new Cache(new RedisAdapter($redis));
-//
-//        $database = new Database(new Postgres($pdo), $cache);
-//        $database->setDefaultDatabase('utopiaTests');
-//        $database->setNamespace('myapp_'.uniqid());
-//
-//        return self::$database = $database;
-//    }
-//}
+
+namespace Utopia\Tests\Adapter;
+
+use PDO;
+use Redis;
+use Utopia\Database\Database;
+use Utopia\Database\Adapter\Postgres;
+use Utopia\Cache\Cache;
+use Utopia\Cache\Adapter\Redis as RedisAdapter;
+use Utopia\Tests\Base;
+
+class PostgresTest extends Base
+{
+    /**
+     * @var Database
+     */
+    static $database = null;
+
+    /**
+     * Return name of adapter
+     *
+     * @return string
+     */
+    static function getAdapterName(): string
+    {
+        return "postgres";
+    }
+
+    /**
+     * @reture Adapter
+     */
+    static function getDatabase(): Database
+    {
+        if(!is_null(self::$database)) {
+            return self::$database;
+        }
+
+        $dbHost = 'postgres';
+        $dbPort = '5432';
+        $dbUser = 'root';
+        $dbPass = 'password';
+
+        $pdo = new PDO("pgsql:host={$dbHost};port={$dbPort};", $dbUser, $dbPass, Postgres::getPDOAttributes());
+        $redis = new Redis();
+        $redis->connect('redis', 6379);
+        $redis->flushAll();
+        $cache = new Cache(new RedisAdapter($redis));
+
+        $database = new Database(new Postgres($pdo), $cache);
+        $database->setDefaultDatabase('utopiaTests');
+        $database->setNamespace('myapp_'.uniqid());
+
+        return self::$database = $database;
+    }
+}

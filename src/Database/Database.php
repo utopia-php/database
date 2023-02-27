23b5738040ccf8465b57f9b3f76ca996ecc5102f
fogelito <fogelshmuel@gmail.com>
2023-02-27 11:45:22 +0200

    validation timeout on find

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -1680,7 +1680,7 @@ class Database
      */
     public function find(string $collection, array $queries = [], ?int $timeout = null): array
     {
-        if(!is_null($timeout) and $timeout <= 0){
+        if(!is_null($timeout) && $timeout <= 0){
             throw new Exception("Timeout must be greater than 0");
         }
 

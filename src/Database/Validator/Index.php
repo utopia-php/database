ecbd4d0eced0048f41e06c1ac433431b8c13a2e1
fogelito <fogelshmuel@gmail.com>
2023-05-29 18:36:22 +0300

    lint

--- src/Database/Validator/Index.php
+++ src/Database/Validator/Index.php
@@ -58,7 +58,7 @@ class Index extends Validator
     {
         foreach ($collection->getAttribute('indexes', []) as $index) {
             $attributes = $index->getAttribute('attributes', []);
-            if(count($attributes) !== count(array_unique($attributes))){
+            if (count($attributes) !== count(array_unique($attributes))) {
                 $this->message = 'Duplicated attributes provided';
                 return false;
             }

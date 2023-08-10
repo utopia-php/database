708d2c1caca5450f6cecf102d2fc875c463dca64
Jake Barnby <jakeb994@gmail.com>
2023-08-10 13:31:23 -0400

    Fix `isEmpty` returning true $id is not selected

--- tests/Database/Adapter/SQLiteTest.php
+++ tests/Database/Adapter/SQLiteTest.php
@@ -66,9 +66,4 @@ class SQLiteTest extends Base
 
         return self::$database = $database;
     }
-
-    public static function killDatabase(): void
-    {
-        self::$database = null;
-    }
 }

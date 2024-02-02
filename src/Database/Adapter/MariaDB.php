87d54b908dd8ebda61bede883669da3cf5bca9ff
Jake Barnby <jakeb994@gmail.com>
2024-02-02 17:28:43 +1300

    Fix deprecation warning

--- src/Database/Adapter/MariaDB.php
+++ src/Database/Adapter/MariaDB.php
@@ -1579,7 +1579,7 @@ class MariaDB extends SQL
                 $orderType = $orderType === Database::ORDER_ASC ? Database::ORDER_DESC : Database::ORDER_ASC;
             }
 
-            $orders[] = "`${attribute}` ${orderType}";
+            $orders[] = "`{$attribute}` {$orderType}";
         }
 
         // Allow after pagination without any order

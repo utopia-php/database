f84fd3b1d95e796fadb7fc9abccff2773a6ae99e
fogelito <fogelshmuel@gmail.com>
2024-04-02 17:12:40 +0300

    Line fix

--- tests/Database/Base.php
+++ tests/Database/Base.php
@@ -7345,6 +7345,7 @@ abstract class Base extends TestCase
             id: 'newMovie',
             onDelete: Database::RELATION_MUTATE_SET_NULL
         );
+
         // Delete child, set parent relationship to null
         static::getDatabase()->deleteDocument('movie', 'movie1');
 

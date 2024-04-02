236fa3a1e1c41e70da42ba64c541d29e15f675bb
fogelito <fogelshmuel@gmail.com>
2024-04-02 18:00:53 +0300

    phpstan ignore

--- tests/e2e/Adapter/Base.php
+++ tests/e2e/Adapter/Base.php
@@ -309,6 +309,7 @@ abstract class Base extends TestCase
          */
         try {
             static::getDatabase()->find('v2', [
+                //@phpstan-ignore-next-line
                 Query::equal('v1', [['doc1']]),
             ]);
             $this->fail('Failed to throw exception');

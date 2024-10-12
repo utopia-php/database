a6de71a7e87369abce1b99f85d7c40be83e4b17b
Matej Bačo <matejbaco2000@gmail.com>
2024-10-12 21:26:08 +0000

    Add safeuards for shared table

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -1325,7 +1325,15 @@ class Database
             $this->deleteRelationship($collection->getId(), $relationship->getId());
         }
 
-        $this->adapter->deleteCollection($id);
+        try {
+            $this->adapter->deleteCollection($id);
+        } catch (DuplicateException $e) {
+            // HACK: Metadata should still be updated, can be removed when null tenant collections are supported.
+            if (!$this->adapter->getSharedTables()) {
+                throw $e;
+            }
+        }
+
 
         if ($id === self::METADATA) {
             $deleted = true;
@@ -1952,7 +1960,17 @@ class Database
             }
         }
 
-        $deleted = $this->adapter->deleteAttribute($collection->getId(), $id);
+        $deleted = null;
+        try {
+            $deleted = $this->adapter->deleteAttribute($collection->getId(), $id);
+        } catch (DuplicateException $e) {
+            // HACK: Metadata should still be updated, can be removed when null tenant collections are supported.
+            if (!$this->adapter->getSharedTables()) {
+                throw $e;
+            }
+
+            $deleted = true;
+        }
 
         if (!$deleted) {
             throw new DatabaseException('Failed to delete attribute');

69e210d4a214d2a463a0fa1df491c9f3d888c4c0
Bradley Schofield <ionicisere@gmail.com>
2024-11-22 12:24:33 +0900

    Address PHP 8.4 Deprecations for PHPStan

--- src/Database/Adapter/Postgres.php
+++ src/Database/Adapter/Postgres.php
@@ -478,12 +478,12 @@ class Postgres extends SQL
      * @param int $size
      * @param bool $signed
      * @param bool $array
-     * @param string $newKey
+     * @param string|null $newKey
      * @return bool
      * @throws Exception
      * @throws PDOException
      */
-    public function updateAttribute(string $collection, string $id, string $type, int $size, bool $signed = true, bool $array = false, string $newKey = null): bool
+    public function updateAttribute(string $collection, string $id, string $type, int $size, bool $signed = true, bool $array = false, ?string $newKey = null): bool
     {
         $name = $this->filter($collection);
         $id = $this->filter($id);

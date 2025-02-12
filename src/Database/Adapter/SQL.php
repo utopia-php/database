40c06a50ae9c1176f705e3853601f29a748bdb61
Jake Barnby <jakeb994@gmail.com>
2025-02-12 17:39:20 +1300

    Fix null uniqueness issues for shared v2

--- src/Database/Adapter/SQL.php
+++ src/Database/Adapter/SQL.php
@@ -1155,7 +1155,7 @@ abstract class SQL extends Adapter
             }
         }
 
-        $tmp = implode(' '. $separator .' ', $conditions);
+        $tmp = implode(' ' . $separator . ' ', $conditions);
         return empty($tmp) ? '' : '(' . $tmp . ')';
     }
 
@@ -1184,18 +1184,10 @@ abstract class SQL extends Adapter
         }
 
         if (!empty($parentAlias) || $parentAlias === '0') {
-            $parentAlias .= '.';
+            $parentAlias = "`{$parentAlias}`.";
         }
 
-        $query = "AND ({$parentAlias}_tenant = :_tenant";
-
-        if ($collection === Database::METADATA) {
-            $query .= " OR {$parentAlias}_tenant IS NULL";
-        }
-
-        $query .= ")";
-
-        return $query;
+        return "AND {$parentAlias}_tenant IN (:_tenant, 0)";
     }
 
     protected function processException(PDOException $e): \Exception

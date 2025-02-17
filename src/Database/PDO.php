d934b991397f99810c986b1e97daaa7e502a8017
Jake Barnby <jakeb994@gmail.com>
2025-02-17 23:20:45 +1300

    Fix stan

--- src/Database/PDO.php
+++ src/Database/PDO.php
@@ -2,10 +2,21 @@
 
 namespace Utopia\Database;
 
+/**
+ * A PDO wrapper that forwards method calls to the internal PDO instance.
+ *
+ * @mixin \PDO
+ */
 class PDO
 {
     protected \PDO $pdo;
 
+    /**
+     * @param string $dsn
+     * @param ?string $username
+     * @param ?string $password
+     * @param array<mixed> $config
+     */
     public function __construct(
         protected string $dsn,
         protected ?string $username,
@@ -20,11 +31,21 @@ class PDO
         );
     }
 
+    /**
+     * @param string $method
+     * @param array<mixed> $args
+     * @return mixed
+     */
     public function __call(string $method, array $args): mixed
     {
         return $this->pdo->{$method}(...$args);
     }
 
+    /**
+     * Create a new connection to the database
+     *
+     * @return void
+     */
     public function reconnect(): void
     {
         $this->pdo = new \PDO(

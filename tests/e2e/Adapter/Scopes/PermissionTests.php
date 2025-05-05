d48a5e003b3a021cac3ffc8a54e341a2090cb240
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-05-05 16:50:36 +0530

    added permission tests

---
+++ tests/e2e/Adapter/Scopes/PermissionTests.php
@@ -0,0 +1,103 @@
+<?php
+
+use Exception;
+use Utopia\Database\Database;
+use Utopia\Database\Document;
+use Utopia\Database\Exception\Authorization as AuthorizationException;
+use Utopia\Database\Helpers\ID;
+use Utopia\Database\Helpers\Permission;
+use Utopia\Database\Helpers\Role;
+use Utopia\Database\Validator\Authorization;
+
+trait PermissionTests
+{
+    public function testReadPermissionsFailure(): Document
+    {
+        Authorization::cleanRoles();
+        Authorization::setRole(Role::any()->toString());
+
+        $document = static::getDatabase()->createDocument('documents', new Document([
+            '$permissions' => [
+                Permission::read(Role::user('1')),
+                Permission::create(Role::user('1')),
+                Permission::update(Role::user('1')),
+                Permission::delete(Role::user('1')),
+            ],
+            'string' => 'text📝',
+            'integer_signed' => -Database::INT_MAX,
+            'integer_unsigned' => Database::INT_MAX,
+            'bigint_signed' => -Database::BIG_INT_MAX,
+            'bigint_unsigned' => Database::BIG_INT_MAX,
+            'float_signed' => -5.55,
+            'float_unsigned' => 5.55,
+            'boolean' => true,
+            'colors' => ['pink', 'green', 'blue'],
+        ]));
+
+        Authorization::cleanRoles();
+
+        $document = static::getDatabase()->getDocument($document->getCollection(), $document->getId());
+
+        $this->assertEquals(true, $document->isEmpty());
+
+        Authorization::setRole(Role::any()->toString());
+
+        return $document;
+    }
+
+    public function testNoChangeUpdateDocumentWithoutPermission(): Document
+    {
+        $document = static::getDatabase()->createDocument('documents', new Document([
+            '$id' => ID::unique(),
+            '$permissions' => [
+                Permission::read(Role::any())
+            ],
+            'string' => 'text📝',
+            'integer_signed' => -Database::INT_MAX,
+            'integer_unsigned' => Database::INT_MAX,
+            'bigint_signed' => -Database::BIG_INT_MAX,
+            'bigint_unsigned' => Database::BIG_INT_MAX,
+            'float_signed' => -123456789.12346,
+            'float_unsigned' => 123456789.12346,
+            'boolean' => true,
+            'colors' => ['pink', 'green', 'blue'],
+        ]));
+
+        $updatedDocument = static::getDatabase()->updateDocument(
+            'documents',
+            $document->getId(),
+            $document
+        );
+
+        // Document should not be updated as there is no change.
+        // It should also not throw any authorization exception without any permission because of no change.
+        $this->assertEquals($updatedDocument->getUpdatedAt(), $document->getUpdatedAt());
+
+        $document = static::getDatabase()->createDocument('documents', new Document([
+            '$id' => ID::unique(),
+            '$permissions' => [],
+            'string' => 'text📝',
+            'integer_signed' => -Database::INT_MAX,
+            'integer_unsigned' => Database::INT_MAX,
+            'bigint_signed' => -Database::BIG_INT_MAX,
+            'bigint_unsigned' => Database::BIG_INT_MAX,
+            'float_signed' => -123456789.12346,
+            'float_unsigned' => 123456789.12346,
+            'boolean' => true,
+            'colors' => ['pink', 'green', 'blue'],
+        ]));
+
+        // Should throw exception, because nothing was updated, but there was no read permission
+        try {
+            static::getDatabase()->updateDocument(
+                'documents',
+                $document->getId(),
+                $document
+            );
+        } catch (Exception $e) {
+            $this->assertInstanceOf(AuthorizationException::class, $e);
+        }
+
+        return $document;
+    }
+}

757827e1edc3a27327ebdc43436e0af48087ec22
fogelito <fogelshmuel@gmail.com>
2025-05-27 11:01:28 +0300

    formatting

--- src/Database/Adapter/SQL.php
+++ src/Database/Adapter/SQL.php
@@ -8,7 +8,6 @@ use Utopia\Database\Adapter;
 use Utopia\Database\Database;
 use Utopia\Database\Document;
 use Utopia\Database\Exception as DatabaseException;
-use Utopia\Database\Exception\Duplicate as DuplicateException;
 use Utopia\Database\Exception\NotFound as NotFoundException;
 use Utopia\Database\Exception\Transaction as TransactionException;
 use Utopia\Database\PDO;

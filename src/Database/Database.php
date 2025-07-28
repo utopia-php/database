5f6c1fb24633141e9ffa4fd7e4b5f6716c4ffd67
fogelito <fogelshmuel@gmail.com>
2025-07-28 15:14:34 +0300

    Add test

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -4108,7 +4108,7 @@ class Database
                 fn () => $this->getDocument($collection->getId(), $id, forUpdate: true)
             ));
 
-            $skipPermissionsUpdate = true;
+            $skipPermissionsUpdate = false;
 
             if ($document->offsetExists('$permissions')) {
                 $originalPermissions = $old->getPermissions();

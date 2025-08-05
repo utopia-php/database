f948a358370432cd923b435f7df600cdc65a5c4a
Jake Barnby <jakeb994@gmail.com>
2025-08-05 20:11:42 +1200

    Fix max 0

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -5233,7 +5233,7 @@ class Database
                 }
             }
 
-            if ($max && ($document->getAttribute($attribute) + $value > $max)) {
+            if (!\is_null($max) && ($document->getAttribute($attribute) + $value > $max)) {
                 throw new LimitException('Attribute value exceeds maximum limit: ' . $max);
             }
 

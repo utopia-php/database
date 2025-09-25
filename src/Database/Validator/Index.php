42eca73d08d9d77fd1f48d3b36e628814c6a69aa
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-09-25 18:59:24 +0530

    updated index validator

--- src/Database/Validator/Index.php
+++ src/Database/Validator/Index.php
@@ -203,6 +203,10 @@ class Index extends Validator
             return true;
         }
 
+        if (!$this->supportForAttributes) {
+            return true;
+        }
+
         $total = 0;
         $lengths = $index->getAttribute('lengths', []);
         $attributes = $index->getAttribute('attributes', []);

69d70131e92169ccc8cb540cbb49df127f634390
fogelito <fogelshmuel@gmail.com>
2025-12-24 17:54:07 +0200

    Use original document

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -8239,14 +8239,13 @@ class Database
      */
     public function decode(QueryContext $context, Document $document, array $selects = []): Document
     {
-        // Build internal attributes lookup
         $internals = [];
+        $schema = [];
+
         foreach (Database::INTERNAL_ATTRIBUTES as $attribute) {
             $internals[$attribute['$id']] = $attribute;
         }
 
-        // Build schema lookup per collection
-        $schema = [];
         foreach ($context->getCollections() as $collection) {
             foreach ($collection->getAttribute('attributes', []) as $attribute) {
                 $key = $attribute->getAttribute('key', $attribute->getAttribute('$id'));
@@ -8259,7 +8258,6 @@ class Database
             $alias = Query::DEFAULT_ALIAS;
             $attributeKey = '';
 
-            // Map select aliases
             foreach ($selects as $select) {
                 if ($select->getAs() === $key) {
                     $attributeKey = $key;
@@ -8268,7 +8266,8 @@ class Database
                     break;
                 }
 
-                if ($select->getAttribute() === $key || $this->adapter->filter($select->getAttribute()) === $key) {
+                if ($select->getAttribute() == $key ||
+                    $this->adapter->filter($select->getAttribute()) == $key) {
                     $alias = $select->getAlias();
                     break;
                 }
@@ -8279,15 +8278,13 @@ class Database
                 throw new \Exception('Invalid query: Unknown Alias context');
             }
 
-            // Find attribute definition
             $attribute = $internals[$key]
                 ?? $schema[$collection->getId()][$this->adapter->filter($key)]
                 ?? null;
 
-            // If attribute is not defined and schemaless is allowed
             if ($attribute === null) {
                 if (!$this->adapter->getSupportForAttributes()) {
-                    $document->setAttribute($key, $value);
+                    $document->setAttribute($key, $value); // schemaless
                 }
                 continue;
             }
@@ -8296,122 +8293,32 @@ class Database
                 $attributeKey = $attribute['$id'];
             }
 
-            $isArray = $attribute['array'] ?? false;
+            $array   = $attribute['array'] ?? false;
             $filters = $attribute['filters'] ?? [];
 
-            // Skip Operator objects
+            // Skip decoding for Operator objects
             if ($value instanceof Operator) {
                 continue;
             }
 
-            // Normalize value to array for uniform filter processing
-            $values = $isArray ? $value : [$value];
+            $value = $array ? $value : [$value];
+            $value = is_null($value) ? [] : $value;
 
-            foreach ($values as $index => $item) {
+            foreach ($value as $index => $node) {
                 foreach (array_reverse($filters) as $filter) {
-                    $item = $this->decodeAttribute($filter, $item, $document, $key);
+                    $node = $this->decodeAttribute($filter, $node, $document, $key);
                 }
-                $values[$index] = $item;
+                $value[$index] = $node;
             }
 
-            // Assign back to document, preserving null values
             $document->setAttribute(
                 $attributeKey,
-                $isArray ? $values : ($values[0] ?? null)
+                $array ? $value : ($value[0] ?? null)
             );
         }
 
         return $document;
     }
-
-    //    public function decode(QueryContext $context, Document $document, array $selects = []): Document
-    //    {
-    //        $internals = [];
-    //        $schema = [];
-    //
-    //        foreach (Database::INTERNAL_ATTRIBUTES as $attribute) {
-    //            $internals[$attribute['$id']] = $attribute;
-    //        }
-    //
-    //        foreach ($context->getCollections() as $collection) {
-    //            foreach ($collection->getAttribute('attributes', []) as $attribute) {
-    //                $key = $attribute->getAttribute('key', $attribute->getAttribute('$id'));
-    //                $key = $this->adapter->filter($key);
-    //                $schema[$collection->getId()][$key] = $attribute->getArrayCopy();
-    //            }
-    //        }
-    //
-    //        $new = $this->createDocumentInstance($context->getMainCollection()->getId(), []);
-    //
-    //        foreach ($document as $key => $value) {
-    //            $alias = Query::DEFAULT_ALIAS;
-    //            $attributeKey = '';
-    //
-    //            foreach ($selects as $select) {
-    //                if ($select->getAs() === $key) {
-    //                    $attributeKey = $key;
-    //                    $key = $select->getAttribute();
-    //                    $alias = $select->getAlias();
-    //
-    //                    break;
-    //                }
-    //
-    //                if ($select->getAttribute() == $key || $this->adapter->filter($select->getAttribute()) == $key) {
-    //                    $alias = $select->getAlias();
-    //
-    //                    break;
-    //                }
-    //            }
-    //
-    //            $collection = $context->getCollectionByAlias($alias);
-    //            if ($collection->isEmpty()) {
-    //                throw new \Exception('Invalid query: Unknown Alias context');
-    //            }
-    //
-    //            $attribute = $internals[$key] ?? null;
-    //
-    //            if (is_null($attribute)) {
-    //                $attribute = $schema[$collection->getId()][$this->adapter->filter($key)] ?? null;
-    //            }
-    //
-    //            if (is_null($attribute)) {
-    //                if (!$this->adapter->getSupportForAttributes()) {
-    //                    $new->setAttribute($key, $value);  /** Schemaless */
-    //                }
-    //
-    //                continue;
-    //            }
-    //
-    //            if (empty($attributeKey)) {
-    //                $attributeKey = $attribute['$id'];
-    //            }
-    //
-    //            $array = $attribute['array'] ?? false;
-    //            $filters = $attribute['filters'] ?? [];
-    //
-    //            // Skip decoding for Operator objects (shouldn't happen, but safety check)
-    //            if ($value instanceof Operator) {
-    //                continue;
-    //            }
-    //
-    //            $value = ($array) ? $value : [$value];
-    //            $value = (is_null($value)) ? [] : $value;
-    //
-    //            foreach ($value as $index => $node) {
-    //                foreach (\array_reverse($filters) as $filter) {
-    //                    $node = $this->decodeAttribute($filter, $node, $new, $key);
-    //                }
-    //
-    //                $value[$index] = $node;
-    //            }
-    //
-    //            $value = ($array) ? $value : $value[0];
-    //
-    //            $new->setAttribute($attributeKey, $value);
-    //        }
-    //
-    //        return $new;
-    //    }
 
     /**
      * Casting

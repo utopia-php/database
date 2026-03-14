54ca3138e0f47732704f5fdd29ab08b3e253c720
Jake Barnby <jakeb994@gmail.com>
2026-03-14 22:49:43 +1300

    (refactor): change Operator method from string to OperatorType enum

--- src/Database/Operator.php
+++ src/Database/Operator.php
@@ -13,27 +13,23 @@ use Utopia\Database\Exception\Operator as OperatorException;
  */
 class Operator
 {
-    protected string $method = '';
-
-    protected string $attribute = '';
-
-    /**
-     * @var array<mixed>
-     */
-    protected array $values = [];
-
     /**
      * Construct a new operator object
      *
      * @param  array<mixed>  $values
      */
-    public function __construct(string $method, string $attribute = '', array $values = [])
-    {
-        $this->method = $method;
-        $this->attribute = $attribute;
-        $this->values = $values;
+    public function __construct(
+        protected OperatorType $method,
+        protected string $attribute = '',
+        protected array $values = [],
+    ) {
     }
 
+    /**
+     * Deep clone operator values that are themselves Operator instances.
+     *
+     * @return void
+     */
     public function __clone(): void
     {
         foreach ($this->values as $index => $value) {
@@ -43,17 +39,29 @@ class Operator
         }
     }
 
-    public function getMethod(): string
+    /**
+     * Get the operator method type.
+     *
+     * @return OperatorType
+     */
+    public function getMethod(): OperatorType
     {
         return $this->method;
     }
 
+    /**
+     * Get the target attribute name.
+     *
+     * @return string
+     */
     public function getAttribute(): string
     {
         return $this->attribute;
     }
 
     /**
+     * Get all operator values.
+     *
      * @return array<mixed>
      */
     public function getValues(): array
@@ -61,6 +69,12 @@ class Operator
         return $this->values;
     }
 
+    /**
+     * Get the first value, or a default if none is set.
+     *
+     * @param mixed $default The fallback value
+     * @return mixed
+     */
     public function getValue(mixed $default = null): mixed
     {
         return $this->values[0] ?? $default;
@@ -68,8 +82,11 @@ class Operator
 
     /**
      * Sets method
+     *
+     * @param OperatorType $method The operator method type
+     * @return self
      */
-    public function setMethod(string $method): self
+    public function setMethod(OperatorType $method): self
     {
         $this->method = $method;
 
@@ -78,6 +95,9 @@ class Operator
 
     /**
      * Sets attribute
+     *
+     * @param string $attribute The target attribute name
+     * @return self
      */
     public function setAttribute(string $attribute): self
     {
@@ -90,6 +110,7 @@ class Operator
      * Sets values
      *
      * @param  array<mixed>  $values
+     * @return self
      */
     public function setValues(array $values): self
     {
@@ -100,6 +121,9 @@ class Operator
 
     /**
      * Sets value
+     *
+     * @param mixed $value The value to set
+     * @return self
      */
     public function setValue(mixed $value): self
     {
@@ -110,72 +134,81 @@ class Operator
 
     /**
      * Check if method is supported
+     *
+     * @param OperatorType|string $value The method to check
+     * @return bool
      */
-    public static function isMethod(string $value): bool
+    public static function isMethod(OperatorType|string $value): bool
     {
+        if ($value instanceof OperatorType) {
+            return true;
+        }
+
         return OperatorType::tryFrom($value) !== null;
     }
 
     /**
      * Check if method is a numeric operation
+     *
+     * @return bool
      */
     public function isNumericOperation(): bool
     {
-        $type = OperatorType::tryFrom($this->method);
-
-        return $type !== null && $type->isNumeric();
+        return $this->method->isNumeric();
     }
 
     /**
      * Check if method is an array operation
+     *
+     * @return bool
      */
     public function isArrayOperation(): bool
     {
-        $type = OperatorType::tryFrom($this->method);
-
-        return $type !== null && $type->isArray();
+        return $this->method->isArray();
     }
 
     /**
      * Check if method is a string operation
+     *
+     * @return bool
      */
     public function isStringOperation(): bool
     {
-        $type = OperatorType::tryFrom($this->method);
-
-        return $type !== null && $type->isString();
+        return $this->method->isString();
     }
 
     /**
      * Check if method is a boolean operation
+     *
+     * @return bool
      */
     public function isBooleanOperation(): bool
     {
-        $type = OperatorType::tryFrom($this->method);
-
-        return $type !== null && $type->isBoolean();
+        return $this->method->isBoolean();
     }
 
     /**
      * Check if method is a date operation
+     *
+     * @return bool
      */
     public function isDateOperation(): bool
     {
-        $type = OperatorType::tryFrom($this->method);
-
-        return $type !== null && $type->isDate();
+        return $this->method->isDate();
     }
 
     /**
      * Parse operator from string
      *
+     * @param string $operator JSON-encoded operator string
+     * @return self
      * @throws OperatorException
      */
     public static function parse(string $operator): self
     {
         try {
             $operator = \json_decode($operator, true, flags: JSON_THROW_ON_ERROR);
-        } catch (\JsonException $e) {
+        } catch (JsonException $e) {
             throw new OperatorException('Invalid operator: '.$e->getMessage());
         }
 
@@ -183,6 +216,7 @@ class Operator
             throw new OperatorException('Invalid operator. Must be an array, got '.\gettype($operator));
         }
 
+        /** @var array<string, mixed> $operator */
         return self::parseOperator($operator);
     }
 
@@ -190,7 +224,7 @@ class Operator
      * Parse operator from array
      *
      * @param  array<string, mixed>  $operator
-     *
+     * @return self
      * @throws OperatorException
      */
     public static function parseOperator(array $operator): self
@@ -203,7 +237,8 @@ class Operator
             throw new OperatorException('Invalid operator method. Must be a string, got '.\gettype($method));
         }
 
-        if (! self::isMethod($method)) {
+        $operatorType = OperatorType::tryFrom($method);
+        if ($operatorType === null) {
             throw new OperatorException('Invalid operator method: '.$method);
         }
 
@@ -215,7 +250,7 @@ class Operator
             throw new OperatorException('Invalid operator values. Must be an array, got '.\gettype($values));
         }
 
-        return new self($method, $attribute, $values);
+        return new self($operatorType, $attribute, $values);
     }
 
     /**
@@ -228,28 +263,27 @@ class Operator
      */
     public static function parseOperators(array $operators): array
     {
-        $parsed = [];
-
-        foreach ($operators as $operator) {
-            $parsed[] = self::parse($operator);
-        }
-
-        return $parsed;
+        return \array_map(self::parse(...), $operators);
     }
 
     /**
+     * Convert this operator to an associative array.
+     *
      * @return array<string, mixed>
      */
     public function toArray(): array
     {
         return [
-            'method' => $this->method,
+            'method' => $this->method->value,
             'attribute' => $this->attribute,
             'values' => $this->values,
         ];
     }
 
     /**
+     * Serialize this operator to a JSON string.
+     *
+     * @return string
      * @throws OperatorException
      */
     public function toString(): string
@@ -264,7 +298,9 @@ class Operator
     /**
      * Helper method to create increment operator
      *
+     * @param int|float $value The amount to increment by
      * @param  int|float|null  $max  Maximum value (won't increment beyond this)
+     * @return self
      */
     public static function increment(int|float $value = 1, int|float|null $max = null): self
     {
@@ -273,13 +309,15 @@ class Operator
             $values[] = $max;
         }
 
-        return new self(OperatorType::Increment->value, '', $values);
+        return new self(OperatorType::Increment, '', $values);
     }
 
     /**
      * Helper method to create decrement operator
      *
+     * @param int|float $value The amount to decrement by
      * @param  int|float|null  $min  Minimum value (won't decrement below this)
+     * @return self
      */
     public static function decrement(int|float $value = 1, int|float|null $min = null): self
     {
@@ -288,67 +326,83 @@ class Operator
             $values[] = $min;
         }
 
-        return new self(OperatorType::Decrement->value, '', $values);
+        return new self(OperatorType::Decrement, '', $values);
     }
 
     /**
      * Helper method to create array append operator
      *
      * @param  array<mixed>  $values
+     * @return self
      */
     public static function arrayAppend(array $values): self
     {
-        return new self(OperatorType::ArrayAppend->value, '', $values);
+        return new self(OperatorType::ArrayAppend, '', $values);
     }
 
     /**
      * Helper method to create array prepend operator
      *
      * @param  array<mixed>  $values
+     * @return self
      */
     public static function arrayPrepend(array $values): self
     {
-        return new self(OperatorType::ArrayPrepend->value, '', $values);
+        return new self(OperatorType::ArrayPrepend, '', $values);
     }
 
     /**
      * Helper method to create array insert operator
+     *
+     * @param int $index The position to insert at
+     * @param mixed $value The value to insert
+     * @return self
      */
     public static function arrayInsert(int $index, mixed $value): self
     {
-        return new self(OperatorType::ArrayInsert->value, '', [$index, $value]);
+        return new self(OperatorType::ArrayInsert, '', [$index, $value]);
     }
 
     /**
      * Helper method to create array remove operator
+     *
+     * @param mixed $value The value to remove
+     * @return self
      */
     public static function arrayRemove(mixed $value): self
     {
-        return new self(OperatorType::ArrayRemove->value, '', [$value]);
+        return new self(OperatorType::ArrayRemove, '', [$value]);
     }
 
     /**
      * Helper method to create concatenation operator
      *
      * @param  mixed  $value  Value to concatenate (string or array)
+     * @return self
      */
     public static function stringConcat(mixed $value): self
     {
-        return new self(OperatorType::StringConcat->value, '', [$value]);
+        return new self(OperatorType::StringConcat, '', [$value]);
     }
 
     /**
      * Helper method to create replace operator
+     *
+     * @param string $search The substring to search for
+     * @param string $replace The replacement string
+     * @return self
      */
     public static function stringReplace(string $search, string $replace): self
     {
-        return new self(OperatorType::StringReplace->value, '', [$search, $replace]);
+        return new self(OperatorType::StringReplace, '', [$search, $replace]);
     }
 
     /**
      * Helper method to create multiply operator
      *
+     * @param int|float $factor The factor to multiply by
      * @param  int|float|null  $max  Maximum value (won't multiply beyond this)
+     * @return self
      */
     public static function multiply(int|float $factor, int|float|null $max = null): self
     {
@@ -357,14 +411,15 @@ class Operator
             $values[] = $max;
         }
 
-        return new self(OperatorType::Multiply->value, '', $values);
+        return new self(OperatorType::Multiply, '', $values);
     }
 
     /**
      * Helper method to create divide operator
      *
+     * @param int|float $divisor The divisor
      * @param  int|float|null  $min  Minimum value (won't divide below this)
-     *
+     * @return self
      * @throws OperatorException if divisor is zero
      */
     public static function divide(int|float $divisor, int|float|null $min = null): self
@@ -377,50 +432,56 @@ class Operator
             $values[] = $min;
         }
 
-        return new self(OperatorType::Divide->value, '', $values);
+        return new self(OperatorType::Divide, '', $values);
     }
 
     /**
      * Helper method to create toggle operator
+     *
+     * @return self
      */
     public static function toggle(): self
     {
-        return new self(OperatorType::Toggle->value, '', []);
+        return new self(OperatorType::Toggle, '', []);
     }
 
     /**
      * Helper method to create date add days operator
      *
      * @param  int  $days  Number of days to add (can be negative to subtract)
+     * @return self
      */
     public static function dateAddDays(int $days): self
     {
-        return new self(OperatorType::DateAddDays->value, '', [$days]);
+        return new self(OperatorType::DateAddDays, '', [$days]);
     }
 
     /**
      * Helper method to create date subtract days operator
      *
      * @param  int  $days  Number of days to subtract
+     * @return self
      */
     public static function dateSubDays(int $days): self
     {
-        return new self(OperatorType::DateSubDays->value, '', [$days]);
+        return new self(OperatorType::DateSubDays, '', [$days]);
     }
 
     /**
      * Helper method to create date set now operator
+     *
+     * @return self
      */
     public static function dateSetNow(): self
     {
-        return new self(OperatorType::DateSetNow->value, '', []);
+        return new self(OperatorType::DateSetNow, '', []);
     }
 
     /**
      * Helper method to create modulo operator
      *
      * @param  int|float  $divisor  The divisor for modulo operation
-     *
+     * @return self
      * @throws OperatorException if divisor is zero
      */
     public static function modulo(int|float $divisor): self
@@ -429,7 +490,7 @@ class Operator
             throw new OperatorException('Modulo by zero is not allowed');
         }
 
-        return new self(OperatorType::Modulo->value, '', [$divisor]);
+        return new self(OperatorType::Modulo, '', [$divisor]);
     }
 
     /**
@@ -437,6 +498,7 @@ class Operator
      *
      * @param  int|float  $exponent  The exponent to raise to
      * @param  int|float|null  $max  Maximum value (won't exceed this)
+     * @return self
      */
     public static function power(int|float $exponent, int|float|null $max = null): self
     {
@@ -445,35 +507,39 @@ class Operator
             $values[] = $max;
         }
 
-        return new self(OperatorType::Power->value, '', $values);
+        return new self(OperatorType::Power, '', $values);
     }
 
     /**
      * Helper method to create array unique operator
+     *
+     * @return self
      */
     public static function arrayUnique(): self
     {
-        return new self(OperatorType::ArrayUnique->value, '', []);
+        return new self(OperatorType::ArrayUnique, '', []);
     }
 
     /**
      * Helper method to create array intersect operator
      *
      * @param  array<mixed>  $values  Values to intersect with current array
+     * @return self
      */
     public static function arrayIntersect(array $values): self
     {
-        return new self(OperatorType::ArrayIntersect->value, '', $values);
+        return new self(OperatorType::ArrayIntersect, '', $values);
     }
 
     /**
      * Helper method to create array diff operator
      *
      * @param  array<mixed>  $values  Values to remove from current array
+     * @return self
      */
     public static function arrayDiff(array $values): self
     {
-        return new self(OperatorType::ArrayDiff->value, '', $values);
+        return new self(OperatorType::ArrayDiff, '', $values);
     }
 
     /**
@@ -481,14 +547,18 @@ class Operator
      *
      * @param  string  $condition  Filter condition ('equals', 'notEquals', 'greaterThan', 'lessThan', 'null', 'notNull')
      * @param  mixed  $value  Value to filter by (not used for 'null'/'notNull' conditions)
+     * @return self
      */
     public static function arrayFilter(string $condition, mixed $value = null): self
     {
-        return new self(OperatorType::ArrayFilter->value, '', [$condition, $value]);
+        return new self(OperatorType::ArrayFilter, '', [$condition, $value]);
     }
 
     /**
      * Check if a value is an operator instance
+     *
+     * @param mixed $value The value to check
+     * @return bool
      */
     public static function isOperator(mixed $value): bool
     {
@@ -503,11 +573,12 @@ class Operator
      */
     public static function extractOperators(array $data): array
     {
+        /** @var array<string, Operator> $operators */
         $operators = [];
         $updates = [];
 
         foreach ($data as $key => $value) {
-            if (self::isOperator($value)) {
+            if ($value instanceof self) {
                 // Set the attribute from the document key if not already set
                 if (empty($value->getAttribute())) {
                     $value->setAttribute($key);

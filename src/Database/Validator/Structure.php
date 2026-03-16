effba5a042aba8894ea37e587effac5848ec42ab
Jake Barnby <jakeb994@gmail.com>
2026-03-16 19:49:31 +1300

    Revert "Merge branch 'fix-mongo-perms'"
    This reverts commit b93a891d4dd6da2857d2bf7db9b4a0fcc8827f68, reversing
    changes made to 1c768ad629984af7f6dbfbbb9e2e922f8cdfb3eb.

--- src/Database/Validator/Structure.php
+++ src/Database/Validator/Structure.php
@@ -52,11 +52,11 @@ class Structure extends Validator
         ],
         [
             '$id' => '$tenant',
-            'type' => Database::VAR_ID,
-            'size' => 0,
+            'type' => Database::VAR_INTEGER, // ? VAR_ID
+            'size' => 8,
             'required' => false,
             'default' => null,
-            'signed' => true,
+            'signed' => false,
             'array' => false,
             'filters' => [],
         ],

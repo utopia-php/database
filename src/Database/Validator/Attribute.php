4490a49ac3f451386ba65c473afe25d36394fdb1
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-03-30 19:45:12 +0530

    big int validator for attribute

--- src/Database/Validator/Attribute.php
+++ src/Database/Validator/Attribute.php
@@ -430,6 +430,7 @@ class Attribute extends Validator
                     Database::VAR_MEDIUMTEXT,
                     Database::VAR_LONGTEXT,
                     Database::VAR_INTEGER,
+                    Database::VAR_BIGINT,
                     Database::VAR_FLOAT,
                     Database::VAR_BOOLEAN,
                     Database::VAR_DATETIME,
